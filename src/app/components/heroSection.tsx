c53629c1e99c0d8edc3fd5044b51e17e3e7bc116
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-02-05 12:57:11 +0500

    Update heroSection.tsx

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
      
       <div className="absolute inset-0">
         <Image
-          src="/images/heroImage.png"
+          src="/images/logo.jpeg"
           alt="Background"
           width={1440}
           height={716}
